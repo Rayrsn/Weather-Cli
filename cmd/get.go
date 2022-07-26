25d8997e127a10858526f7dbbe0781a1ee4bb63c
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2022-07-27 00:20:20 +0300

    Added weather code interpretation

--- cmd/get.go
+++ cmd/get.go
@@ -14,7 +14,6 @@ import (
 	"strings"
 
 	"github.com/dustin/go-humanize"
-
 	"github.com/spf13/cobra"
 )
 
@@ -59,13 +58,21 @@ var getCmd = &cobra.Command{
 		var FetchedTemperature = forecastData["current_weather"].(map[string]interface{})["temperature"]
 		var FetchedWindSpeed = forecastData["current_weather"].(map[string]interface{})["windspeed"]
 		var FetchedWindDirection = forecastData["current_weather"].(map[string]interface{})["winddirection"]
-
+		var FetchedWeathercode = forecastData["current_weather"].(map[string]interface{})["weathercode"]
+		fmt.Println(FetchedWeathercode)
+		fmt.Println(translateweathercode(fmt.Sprintf("%v", FetchedWeathercode)))
 		if cmd.Flag("raw").Value.String() == "true" {
-			json, err := json.Marshal(cityinfoData)
+			jsn, err := json.Marshal(cityinfoData)
 			if err != nil {
 				log.Fatalln(err)
 			}
-			os.Stdout.Write(json)
+			os.Stdout.Write(jsn)
+			fmt.Println()
+			jsn, err = json.Marshal(cityinfoData)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			os.Stdout.Write(jsn)
 		} else {
 			printer(FetchedCityName,
 				FetchedCountryName,
@@ -77,12 +84,24 @@ var getCmd = &cobra.Command{
 				FetchedTemperature,
 				FetchedWindSpeed,
 				FetchedWindDirection,
+				translateweathercode(fmt.Sprintf("%v", FetchedWeathercode)),
 			)
 		}
 	},
 }
 
-func printer(Name interface{}, Country interface{}, Latitude interface{}, Longitude interface{}, Timezone interface{}, PopulationFloat interface{}, PopulationInt int64, Temperature interface{}, WindSpeed interface{}, WindDirection interface{}) {
+func printer(Name interface{},
+	Country interface{},
+	Latitude interface{},
+	Longitude interface{},
+	Timezone interface{},
+	PopulationFloat interface{},
+	PopulationInt int64,
+	Temperature interface{},
+	WindSpeed interface{},
+	WindDirection interface{},
+	WeatherCode string,
+) {
 	fmt.Printf("City/Country: %s/%s\n", Name, Country)
 	fmt.Printf("Latitude: %f\n", Latitude)
 	fmt.Printf("Longitude: %f\n", Longitude)
@@ -92,6 +111,71 @@ func printer(Name interface{}, Country interface{}, Latitude interface{}, Longit
 	fmt.Printf("	Temperature: %.1f°\n", Temperature)
 	fmt.Printf("	Wind Direction: %.0f°\n", WindDirection)
 	fmt.Printf("	Wind Speed: %.1f Km/h\n", WindSpeed)
+	fmt.Printf("	Weather Condition: %s\n", WeatherCode)
+}
+
+func translateweathercode(code string) string {
+	switch code {
+	case "0":
+		return "Clear Sky"
+	case "1":
+		return "Mainly Clear"
+	case "2":
+		return "Partly Cloudy"
+	case "3":
+		return "Overcast"
+	case "45":
+		return "Fog"
+	case "48":
+		return "Depositing Rime Fog"
+	case "51":
+		return "Light Drizzle"
+	case "53":
+		return "Moderate Drizzle"
+	case "55":
+		return "Dense Drizzle"
+	case "56":
+		return "Light Freezing Drizzle"
+	case "57":
+		return "Dense Freezing Drizzle"
+	case "61":
+		return "Slight Rain"
+	case "63":
+		return "Moderate Rain"
+	case "65":
+		return "Heavy Rain"
+	case "66":
+		return "Light Freezing Rain"
+	case "67":
+		return "Heavy Freezing Rain"
+	case "71":
+		return "Slight Snow fall"
+	case "73":
+		return "Moderate Snow fall"
+	case "75":
+		return "Heavy Snow fall"
+	case "77":
+		return "Snow Grains"
+	case "80":
+		return "Slight Rain showers"
+	case "81":
+		return "Moderate Rain showers"
+	case "82":
+		return "Violent Rain showers"
+	case "85":
+		return "Slight Snow Showers"
+	case "86":
+		return "Heavy Snow Showers"
+	case "95":
+		return "Thunderstorm"
+	case "96":
+		return "Thunderstorm With Light Hail"
+	case "99":
+		return "Thunderstorm With Heavy Hail"
+
+	default:
+		return "Unknown"
+	}
 }
 
 func init() {
